87267b77c2e6acf472a46846e2d55b79c8efc415
JaimeGuzmanSantoyo <jaimeguzman40@aragon.unam.mx>
2024-08-27 18:46:26 -0600

    IMC TAREA

---
+++ tarea1.js
@@ -0,0 +1,13 @@
+let nombreusuaurii = prompt("ingrese su nombre: ")
+
+let edad= prompt("ingrese su edad: ")
+
+let Estatura=prompt("igrese su estatura: ")
+let peso= prompt("ingrese su peso: ")
+
+
+let estatura2= Estatura* Estatura
+console.log("resultado de la mutiplicacion de la estatura  "+ estatura2)
+let imc = peso / estatura2
+
+console.log("este el imc "+imc)
